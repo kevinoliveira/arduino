3564b28e4dac4123c9158b7fd1b3a8cabc089eef
Kevin Oliveira <kevin.oliveira@live.com>
2020-03-14 17:05:44 -0300

    auto define max and min

--- exam.ino
+++ exam.ino
@@ -1,16 +1,34 @@
-// CONSTANTS
+// LDR
 int LDR_PIN = A0;
-
-// DATA
 int ldr_value = 0;
+int min_ldr_value = 1023;
+int max_ldr_value = 0;
+int ldr_level = 0; // from 0 to 9
 
-void setup()
-{
+void ldr_handler(int raw_ldr_value) {
+    ldr_value = raw_ldr_value;
+    if (raw_ldr_value >= max_ldr_value) max_ldr_value = raw_ldr_value;
+    if (raw_ldr_value <= min_ldr_value) min_ldr_value = raw_ldr_value;
+
+    pretty_log_int("raw: ",raw_ldr_value);
+    pretty_log_int("min: ",min_ldr_value);
+    pretty_log_int("max: ",max_ldr_value);
+    blank_line();
+}
+
+// HELPERS
+void pretty_log_int(char * str,int value) {
+    Serial.print(str);Serial.println(value);
+}
+void blank_line(){
+    Serial.println("");
+}
+
+void setup() {
     Serial.begin(9600);
 }
 
-void loop()
-{
-    ldr_value = analogRead(LDR_PIN);
-    Serial.println(ldr_value);
+void loop() {
+    ldr_handler(analogRead(LDR_PIN));
+    delay(300);
 }
